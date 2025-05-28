eca8d00829079d9302201307da4c11457ad9e4bf
Shehzadnazir456 <shehzadnazir456@gmail.com>
2025-05-28 17:27:24 +0500

    second commit shehzad

--- ProjectCalender.py
+++ ProjectCalender.py
@@ -4,3 +4,6 @@ def is_leap(year):
     """Return True if leap year."""
     return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
+def display_year_calendar(year):
+    """Print the full calendar for the given year."""
+    print("=" * 400)
